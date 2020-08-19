d0d029ea34cb0c86a301890d2d86bb695422b71a
nathankys <kangys1245@gmail.com>
2020-08-19 17:36:38 +0900

    Update 203p_type_of_main_function.c

--- KNK_incorrect/CHAPTER_9_FUNCTION/203p_type_of_main_function.c
+++ KNK_incorrect/CHAPTER_9_FUNCTION/203p_type_of_main_function.c
@@ -3,4 +3,15 @@
 char main(){
 	printf("type of main function doesn't has to be \'int\'\n");
 	
+	return '0';
 }
+
+/*
+double main(){
+	printf("type of main function doesn't has to be \'int\'\n");
+	
+	return 0;
+}
+
+setting the type of main function as 'double' also allowed
+*/
