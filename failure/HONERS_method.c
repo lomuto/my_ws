554af7567e5dfb8d6a77f652ee146901d9644f56
NathanKYS <kangys1245@gmail.com>
2020-08-26 22:38:04 +0900

    Update HONERS_method at failure dir

--- failure/HONERS_method.c
+++ failure/HONERS_method.c
@@ -6,8 +6,9 @@
 
 #include <stdio.h>
 int* n; 
-int k= 0;
+int k= 0;	// 우선 하나 해결. 전역변수에서 포인터 이렇게 선언 못함. 자세하게 왜인지는 모름
 n = &k;
+
 int f(int, int*);
 
 int main() {
@@ -20,6 +21,6 @@ int main() {
 
 int f(int x, int* n) {
     int constant[] = { -6,7,-1,-5 };
-      if ((*n) == 4) return 3 * x + 2;
+      if (*n == 4) return 3 * x + 2;
     return f(x,&n) * x + constant[(*n)++];
 }
