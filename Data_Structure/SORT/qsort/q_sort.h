1d64fcadb7a40828e605b8e8e935a9140e31580e
vandivici <kangys1245@gmail.com>
2021-05-15 19:40:37 +0900

    Add critical issue in qsort

--- Data_Structure/SORT/qsort/q_sort.h
+++ Data_Structure/SORT/qsort/q_sort.h
@@ -10,7 +10,7 @@ void Q_sort(int *arr, int st, int end) {
     if (st >= end) // st:0, end:0 에서 무한호출. st>end 가 아니라`st>=end` 해줘야한다
         return;
     int target = arr[st];
-    int left = st;
+    int left = st; // 만약 left = st+1 해줄꺼면 22줄에 스왑될 때 end-st =1 일 때 정렬되어있어도 바뀔 수 잇음!!
     int right = end;
     while (left < right) {
         while (target < arr[right]) // 기준점이 arr[st]라 배열범위 안벗어남
