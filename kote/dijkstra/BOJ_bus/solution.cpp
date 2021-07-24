66c00dd37720ad1f927c792f03c5edf4cb3adf46
vandivici <kangys1245@gmail.com>
2021-07-24 13:03:24 +0900

    Add BOJ bus

---
+++ kote/dijkstra/BOJ_bus/solution.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int INF = 1000000001;
+vector<vector<pair<int, int>>> bus;
+vector<int> distances;
+int vertexCnt;
+int edgeCnt;
+
+void dijkstra(int src) {
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    distances[src] = 0;
+    pq.push(pair<int, int>{0, src});
+
+    while (!pq.empty()) {
+        int currentVertex = pq.top().second;
+        int distanceToCurrentVertex = pq.top().first;
+        pq.pop();
+
+        if (distances[currentVertex] < distanceToCurrentVertex) {
+            continue;
+        }
+
+        for (pair<int, int> adjacent : bus[currentVertex]) {
+            int candidateVertex = adjacent.first;
+            int weightOfCandidateVertex = adjacent.second;
+
+            if (distances[candidateVertex] > weightOfCandidateVertex + distanceToCurrentVertex) {
+                distances[candidateVertex] = weightOfCandidateVertex + distanceToCurrentVertex;
+                pq.push(pair<int, int>{distances[candidateVertex], candidateVertex});
+            }
+        }
+    }
+}
+
+int main() {
+    cin >> vertexCnt >> edgeCnt;
+    distances.resize(vertexCnt + 1, INF);
+    bus.resize(vertexCnt + 1);
+
+    int from, to, weight;
+    for (int i = 0; i < edgeCnt; i++) {
+        cin >> from >> to >> weight;
+        bus[from].push_back(pair<int, int>{to, weight});
+    }
+
+    int src, dest;
+    cin >> src >> dest;
+    dijkstra(src);
+
+    cout << distances[dest] << '\n';
+}
