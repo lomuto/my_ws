34091f2a67278fb6e8d59b77a34306086b732a06
lomuto <kangys1245@gmail.com>
2021-11-20 21:45:36 +0900

    Remove properties from git

--- springBoot/amigosTutorial/src/main/java/com/example/demo/student/StudentService.java
+++ springBoot/amigosTutorial/src/main/java/com/example/demo/student/StudentService.java
@@ -1,2 +1,25 @@
-package com.example.demo.student;public class StudentService {
+package com.example.demo.student;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    public List<Student> getStudents() {
+        return List.of(
+                new Student(
+                        1L,    // L for Long explicitly
+                        "Mariam",
+                        "mariam.jamal@gmail.com",
+                        LocalDate.of(2000, Month.JANUARY, 5),
+                        21
+                )
+        );
+    }
+
 }
