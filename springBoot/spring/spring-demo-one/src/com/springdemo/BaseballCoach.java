6be4c3962a8668d28f1d6fede1bad7c9a10afb6b
lomuto <kangys1245@gmail.com>
2021-12-27 21:45:14 +0900

    Add dependency injetion

--- springBoot/spring/spring-demo-one/src/com/springdemo/BaseballCoach.java
+++ springBoot/spring/spring-demo-one/src/com/springdemo/BaseballCoach.java
@@ -2,6 +2,12 @@ package com.springdemo;
 
 public class BaseballCoach implements Coach {
 
+    private FortuneService theFortuneService = new HappyFortuneService();
+
+    public BaseballCoach(FortuneService theFortuneService) {
+        this.theFortuneService = theFortuneService;
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Spend 30 min on batting practice";
