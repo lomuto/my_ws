112d2bbc18a0d50b9bfb52fe0ff49d30ead9df27
Yunseok Kang <kang@Yunseokui-MacBookPro.local>
2021-12-27 20:31:41 +0900

    Add spring main class

--- springBoot/spring/spring-demo-one/src/com/springdemo/HelloSpringApp.java
+++ springBoot/spring/spring-demo-one/src/com/springdemo/HelloSpringApp.java
@@ -1,2 +1,20 @@
-package com.springdemo;public class HelloSpringApp {
+package com.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloSpringApp {
+    public static void main(String[] args) {
+        // load ths spring configuration file
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // retrieve bean from spring container
+        Coach coach = context.getBean("myCoach", Coach.class);
+
+        // call method from bean
+        System.out.println(coach.getDailyWorkout());
+
+        // close the context
+        context.close();
+    }
 }
