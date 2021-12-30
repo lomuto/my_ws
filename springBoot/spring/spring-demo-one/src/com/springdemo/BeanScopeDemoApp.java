d33ab78ba2f4bd59e18f5621a35242f2b4a10d39
lomuto <kangys1245@gmail.com>
2021-12-30 18:39:17 +0900

    feat: Add codes for default bean scope test(singleton)

---
+++ springBoot/spring/spring-demo-one/src/com/springdemo/BeanScopeDemoApp.java
@@ -0,0 +1,22 @@
+package com.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeDemoApp {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+
+        Coach coach = context.getBean("myCoach", Coach.class);
+
+        Coach alphaCoach = context.getBean("myCoach", Coach.class);
+
+        System.out.println(coach == alphaCoach);
+
+        System.out.println(coach);
+
+        System.out.println(alphaCoach);
+
+        context.close();
+    }
+}
