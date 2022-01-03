51abfb618c9382e72c6496deea5025dff2eaf24a
lomuto <kangys1245@gmail.com>
2022-01-03 20:40:24 +0900

    feat: Test DI inference in multiple interface implementatip

---
+++ springBoot/spring/spring-demo-annotation/src/com/springdemo/RandomFortuneService.java
@@ -0,0 +1,20 @@
+package com.springdemo;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+
+@Component
+public class RandomFortuneService implements FortuneService{
+    private static String[] fortuneServices = {
+            "Fortune 1",
+            "Fortune 2",
+            "Fortune 3",
+            "Fortune 4"
+    };
+
+    @Override
+    public String getFortune() {
+        return fortuneServices[new Random().nextInt(4)];
+    }
+}
