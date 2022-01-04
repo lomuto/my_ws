c791102442c8e85d3c05b71e2e5385d7eb319242
lomuto <kangys1245@gmail.com>
2022-01-04 16:52:59 +0900

    Init code configuration

---
+++ springBoot/spring/spring-demo-codeConfig/src/com/springdemo/App.java
@@ -0,0 +1,12 @@
+package com.springdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(Config.class);
+
+
+    }
+}
---
+++ springBoot/spring/spring-demo-codeConfig/src/com/springdemo/TennisCoach.java
@@ -0,0 +1,41 @@
+package com.springdemo;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+/*
+    Default Bean ID is same class name with lower case of first character
+ */
+@Component
+public class TennisCoach implements Coach {
+
+    private FortuneService fortuneService;
+
+    public TennisCoach() {
+        System.out.println(
+                String.format("Field FortuneService is not available right now %s", fortuneService)
+        );
+    }
+
+    @Autowired
+    public TennisCoach(
+            @Qualifier("randomFortuneService") FortuneService theFortuneService
+    ) {
+        this.fortuneService = theFortuneService;
+        System.out.println(
+                String.format("Field FortuneService is now available %s", fortuneService)
+        );
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice your backhand volley";
+    }
+
+    public String getFortune() {
+        return fortuneService.getFortune();
+    }
+}
